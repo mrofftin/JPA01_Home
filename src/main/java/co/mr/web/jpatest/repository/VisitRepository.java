53a22dd56fbd4d6c01003f8b4e1cddd13bc6f642
mrofftin <mrofftin@gmail.com>
2022-01-26 09:00:29 +0900

    JPA 기본 Paging, Query

--- src/main/java/co/mr/web/jpatest/repository/VisitRepository.java
+++ src/main/java/co/mr/web/jpatest/repository/VisitRepository.java
@@ -1,7 +1,13 @@
 package co.mr.web.jpatest.repository;
 
 import co.mr.web.jpatest.entity.VisitBook;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 /* Spring Data JPA는 JpaRepository를 상속하는 인터페이스 선언만으로
 모든 DB연동처리를 SQL없이 처리할 수 있게된다. 즉, 실제 동작시에 내부적으로 해당 인터페이스에 맞는
@@ -12,4 +18,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // Spring Data JPA는 이렇게 인터페이스 선언만으로 자동으로 Bean을 등록된다.
 // (내부적으로 스프링이 인터페이스 타입에 맞는 객체를 생성해서 빈으로 등록한다.)
 public interface VisitRepository extends JpaRepository<VisitBook, Long> {
+
+//    List<VisitBook> findByVnoBetweenOrderByVnoDesc(Long from, Long to);
+
+    List<VisitBook> findByVnoBetweenOrderByVnoDesc(Long from, Long to);
+    Page<VisitBook> findByVnoBetween(Long from, Long to, Pageable pageable);
+
+    // num보다 작은 데이터를 삭제한다는 의미
+    void deleteVisitBookByVnoLessThan(Long num);
 }
--- src/test/java/co/mr/web/jpatest/repository/VisitRepositoryTest.java
+++ src/test/java/co/mr/web/jpatest/repository/VisitRepositoryTest.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
+import org.springframework.test.annotation.Commit;
 
 
+import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
 
@@ -72,19 +76,24 @@ class VisitRepositoryTest {
         System.out.println(visitRepository.save(update_text));
     }
 
-    @Test
-    public void testDelete() {
-        Long vno = 100L;
+//    @Test
+//    public void testDelete() {
+//        Long vno = 90L;
+//
+//        visitRepository.deleteById(vno);
+//    }
 
-        visitRepository.deleteById(vno);
-    }
+    // Spring Data JPA를 이용해서 페이지 처리할 때는 반드시 0부터 시작
+    // 페이지 처리는 Pageble인터페이스를 이용(페이지 처리에 필요한 정보를 전달하는 용도)
+    // 실제 객체를 생성할 때는 PageRequest객체를 사용한다. new를 이용하지 않고 of를 이용해서 처리
+
 
     @Test
     public void testPageDefault() {
         // 1페이지에 데이터 10개를 가져오기 위해 0, 10 파라미터 설정
-        PageRequest pageRequest = PageRequest.of(0, 10);
+        Pageable pageable = PageRequest.of(0, 10);
 
-        Page<VisitBook> result = visitRepository.findAll(pageRequest);
+        Page<VisitBook> result = visitRepository.findAll(pageable);
 
         System.out.println(result);
 
@@ -117,10 +126,10 @@ class VisitRepositoryTest {
         Sort sortAll = sort1.and(sort2); // and를 이용한 연결
 
 //        PageRequest pageRequest = PageRequest.of(0, 10, sort1);
-        PageRequest pageRequest = PageRequest.of(0, 10, sortAll);
+        Pageable pageable = PageRequest.of(0, 10, sortAll);
 
 
-        Page<VisitBook> result = visitRepository.findAll(pageRequest);
+        Page<VisitBook> result = visitRepository.findAll(pageable);
 
         // get() : Stream<엔티티 타입 >반환
         result.get().forEach(visitBook -> {
@@ -128,6 +137,34 @@ class VisitRepositoryTest {
         });
     }
 
+    @Test
+    public void testQueryMethod() {
+
+        List<VisitBook> list = visitRepository.findByVnoBetweenOrderByVnoDesc(30L, 40L);
+
+        for (VisitBook visitBook : list) {
+            System.out.println(visitBook);
+        }
+    }
+
+    @Test
+    public void testQueryMethodWithPageable(){
+        List<VisitBook> list = visitRepository.findByVnoBetweenOrderByVnoDesc(30L, 40L);
+
+        Pageable pageable = PageRequest.of(0, 20, Sort.by("vno").descending());
+
+        Page<VisitBook> result = visitRepository.findByVnoBetween(10L, 50L, pageable);
+
+        result.get().forEach(visitBook -> System.out.println(visitBook));
+
+    }
+
+    @Test
+    @Commit
+    @Transactional
+    public void testDeleteQueryMethods() {
+        visitRepository.deleteVisitBookByVnoLessThan(10L);
+    }
 
 
 
